abe251d73be89ed37aa2bc64933bb4af41b4c593
Marleen Rijksen <marleenrijksen@gmail.com>
2018-06-26 17:40:35 +0200

    updated network.py and presentation

--- Code/network_model.py
+++ Code/network_model.py
@@ -20,21 +20,8 @@ def create_degree_sequence(n, sfunction=None, max_tries=50, **kwds):
         Called as "sfunction(n,**kwds)".
     max_tries: int
         Max number of attempts at creating valid degree sequence.
-
-    Notes
-    -----
-
-    Repeatedly create a degree sequence by calling sfunction(n,**kwds)
-    until achieving a valid degree sequence. If unsuccessful after
-    max_tries attempts, raise an exception.
-    
-    For examples of sfunctions that return sequences of random numbers,
-    see networkx.Utils.
-
-    Examples
-    --------
-    >>> from networkx.utils import uniform_sequence, create_degree_sequence
-    >>> seq=create_degree_sequence(10,uniform_sequence)
+        
+    source: https://networkx.github.io/documentation/networkx-1.10
     """
     tries=0
     max_deg=n
@@ -48,6 +35,107 @@ def create_degree_sequence(n, sfunction=None, max_tries=50, **kwds):
         tries+=1
     raise nx.NetworkXError("Exceeded max (%d) attempts at a valid sequence."%max_tries)
 
+def power_law_graph(exp, num, seed=1234, plot=False):
+    """
+    Function which creates power law graph based on a number of nodes (num),
+    an exponent (exp) and a seed. 
+    
+    source: http://nbviewer.jupyter.org/gist/Midnighter/248f1a5d8c21b39525ae
+    """
+    
+    print("Power law exponent = -{0:.2f}".format(exp))
+    
+    # create graph
+    sequence = create_degree_sequence(num, powerlaw_sequence, exponent=exp)
+    graph = nx.configuration_model(sequence, seed=seed)
+
+    # count parallel edges and avoid counting A-B as well as B-A
+    num_par = sum(
+        len(graph[node][neigh]) for node in graph
+        for neigh in graph.neighbors(node)) // 2
+    print("Power law graph has {0:d} parallel edges".format(num_par))
+    loops = graph.selfloop_edges()
+
+    # remove them
+    graph = nx.Graph(graph)
+    graph.remove_edges_from(loops)
+    
+    # get largest connected component
+    # unfortunately, the iterator over the components is not guaranteed to be sorted by size
+    components = sorted(nx.connected_components(graph), key=len, reverse=True)
+    lcc = graph.subgraph(components[0])
+    print("Size of largest connected component = {0:d}".format(len(lcc)))
+    
+    if plot == True:
+
+        # new degree sequence
+        simple_seq = [deg for (node, deg) in lcc.degree()]
+
+        # create histograms
+        counts = np.bincount(sequence)
+        mask = (counts > 0)
+        plt.figure()
+        plt.plot(
+            np.arange(len(counts))[mask],
+            counts[mask] / counts.sum(),
+            "o",
+            label="MultiGraph")
+        simple_counts = np.bincount(simple_seq)
+        mask = (simple_counts > 0)
+
+        # distribution is shifted for visibility
+        plt.plot(
+            np.arange(len(simple_counts))[mask],
+            simple_counts[mask] / simple_counts.sum() / 10.0,
+            "o",
+            label="Simple LCC")
+
+        # plot distribution
+        x = np.arange(1, len(counts))
+        plt.plot(x, np.power(x, -exp))
+        plt.xlabel(r"Degree $k$")
+        plt.xscale("log")
+        plt.ylabel(r"Probability $P(k)$")
+        plt.yscale("log")
+        plt.title(r"$N = {0:d}, \\quad \\lambda = {1:.2f}$".format(num, exp))
+        plt.legend(loc="best")
+        plt.show()
+        nx.powerlaw_cluster_graph
+
+        # visualize network
+        plt.figure()
+        pos = nx.spring_layout(graph)
+        nx.draw(graph, pos, node_color='b', node_size=10, with_labels=False)
+        plt.show()
+    return graph
+
+def random_network(p, num, seed=1234, plot=False):
+    """
+    Function which creates random network based on edge probability (p),
+    a given number of nodes (num) and a seed. 
+    """
+    
+    # create graph
+    graph = nx.fast_gnp_random_graph(num, p, seed=seed, directed=False)
+
+    # plot
+    if plot == True:
+        plt.figure()
+        pos = nx.spring_layout(graph)
+        nx.draw(graph, pos, node_color='b', node_size=5, with_labels=False)
+        plt.show()
+
+        plt.figure()
+        degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)
+        # print "Degree sequence", degree_sequence
+        dmax = max(degree_sequence)
+
+        plt.semilogy(degree_sequence, 'b-', marker='o')
+        plt.title("Degree rank plot")
+        plt.ylabel("degree")
+        plt.xlabel("rank")
+        plt.show()
+    return graph
 
 class Individual():
     """ 
@@ -64,17 +152,29 @@ class Individual():
         self.disease_status = 0
         self.time_since_infection = -1
 
-def network_model(beta, tau, nu, mu, init, num_steps ,graph, doInit = False):
+
+def network_model(beta, tau, nu, mu, init, num_steps, graph, doInit = False, disable_progress=False):
+    """
+    Function which runs disease spreading model on specified network.
+    
+    beta = transmission probability
+    tau = probability of treatment
+    nu = probability of recovering spontaneously
+    init = initial number of infecteds (with sensitive strain)
+    num_steps = number of iterations to run model
+    graph = an initialized graph, e.g. random or scale-free
+    doInit = boolean specifying to initialize or not
+    """
+    
+    # arrays/set containing number of diseased
     num_infected  = np.zeros(num_steps)
     num_res = np.zeros(num_steps)
-
     infected = set()
     
+    # initialization of infected individuals
     if doInit:
         for i in range(len(graph)):
             graph.node[i]["Data"].disease_status = 0
-
-        
         for i in init:
             graph.node[i]["Data"].disease_status = 1
             infected.add(i)
@@ -83,9 +183,8 @@ def network_model(beta, tau, nu, mu, init, num_steps ,graph, doInit = False):
             if graph.node[i]["Data"].disease_status:
                 infected.add(i)
       
-    
     # iterate over time
-    for t in tqdm(range(num_steps), position = 0):
+    for t in tqdm(range(num_steps), position = 0, disable=disable_progress):
         infected_copy = infected.copy()
 
         # iterate over infecteds 
@@ -124,5 +223,4 @@ def network_model(beta, tau, nu, mu, init, num_steps ,graph, doInit = False):
                 tot += 1
         num_res[t] = tot 
         num_infected[t] = len(infected)
-        
-    return (num_res, num_infected)
+    return (num_res, num_infected, graph)
